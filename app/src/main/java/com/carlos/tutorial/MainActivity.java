d96508fb11d74ea237edf1e05a8de9027365a5fb
Carlos <Cstaz12@yahoo.com>
2016-11-05 21:52:25 -0400

    Made text change when the button is clicked.

--- app/src/main/java/com/carlos/tutorial/MainActivity.java
+++ app/src/main/java/com/carlos/tutorial/MainActivity.java
@@ -8,6 +8,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -48,5 +49,10 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void simple_click(View view) {
+        TextView t = (TextView) findViewById(R.id.textView);
+        t.setText("Goodbye!");
     }
 }
